a0d145373a85007c6a8a4d2d0725ea2c040ec64c
Winter Cyan <wintercyan98@gmail.com>
2020-09-13 23:37:09 +0800

    single_renderer added.

--- InNetworkRenderer.py
+++ InNetworkRenderer.py
@@ -18,7 +18,8 @@ def generate_direction(batch_size, low_eps=0.001, high_eps=0.05):
     phi = 2 * math.pi * r2
     x = r * torch.cos(phi)
     y = r * torch.sin(phi)
-    z = torch.sqrt(1.0 - torch.square(r))
+    # z = torch.sqrt(1.0 - torch.square(r))
+    z = torch.sqrt(1.0 - r**2)
     final_vec = torch.cat([x,y,z], dim=-1)  # [N, 3], every vec for a single sample in the batch
     return final_vec
 
@@ -31,8 +32,9 @@ def generate_distance(batch_size):
 
 def norm_vec(x):
     # vec: [N,3]
-    len = torch.sqrt(torch.sum(torch.square(x), dim=-1, keepdim=True))
-    return torch.div(x, len)
+    # len = torch.sqrt(torch.sum(torch.square(x), dim=-1, keepdim=True))
+    length = torch.sqrt(torch.sum(x**2, dim=-1, keepdim=True))
+    return torch.div(x, length)
 
 
 def dot_vec(x,y):
@@ -43,6 +45,7 @@ def dot_vec(x,y):
 # svbrdf: [N,12,H,W], normal, diffuse, roughness, specular
 # wi, wo: [N, 3]
 def tf_render(svbrdf, wi, wo):
+    # params: [N,3,H,W]
     normal = svbrdf[:, 0:3, :, :]
     diffuse = torch.clamp(map_to_img(svbrdf[:, 3:6, :, :]), min=0.0, max=1.0)
     roughness = torch.clamp(map_to_img(svbrdf[:, 6:9, :, :]), min=0.001, max=1.0)
@@ -64,16 +67,79 @@ def tf_render(svbrdf, wi, wo):
     diffuse_rendered = diffuse * (1.0 - specular)/math.pi
 
     # D
-    alpha = torch.square(roughness)
-    denominator = 1.0/torch.clamp((torch.square(NdotH_pos)*(torch.square(alpha)-1.0)+1.0), min=0.001)
-    D = torch.square(alpha*denominator)/math.pi
+    # alpha = torch.square(roughness)
+    alpha = roughness**2
+    # denominator = 1.0/torch.clamp((torch.square(NdotH_pos)*(torch.square(alpha)-1.0)+1.0), min=0.001)
+    denominator = 1.0/torch.clamp(((NdotH_pos**2)*((alpha**2)-1.0)+1.0), min=0.001)
+    # D = torch.square(alpha*denominator)/math.pi
+    D = ((alpha*denominator)**2)/math.pi
 
     # F
     sphg = torch.pow(((-5.55473*VdotH_pos)-6.98316)*VdotH_pos, exponent=2.0)
     F = specular+(1.0-specular)*sphg
 
     # G
-    k = torch.square(roughness)/2.0
+    # k = torch.square(roughness)/2.0
+    k = (roughness**2)/2.0
+    g1 = 1.0/torch.clamp((NdotL_pos*(1.0-k)+k), min=0.001)
+    g2 = 1.0/torch.clamp((NdotV_pos*(1.0-k)+k), min=0.001)
+    G = g1 + g2
+
+    specular_rendered = F*(G*D*0.25)
+    result = specular_rendered
+    result = torch.add(result, diffuse_rendered)
+
+    light_intensity = 1.0
+    light_factor = light_intensity * math.pi
+
+    result = result * light_factor
+
+    # This division is to compensate for the cosinus distribution of the intensity in the rendering
+    # result = result * NdotL_pos / tf.expand_dims(tf.maximum(wiNorm[:,:,:,2], 0.001), axis=-1)
+    # result = result * NdotL_pos / torch.unsqueeze(torch.clamp(wi_norm[:, :, :, 2], min=0.001), dim=-1)
+
+    # return [result, D, G, F, diffuse_rendered, diffuse]
+    return result
+
+def single_renderer(svbrdf, wi, wo):
+    # svbrdf, 1 tensor of 4 params: normal albedo, roughness, specular, [12,288,288]
+    # wi, wo: [3]
+    # params: [3,288,288]
+    normal = svbrdf[0:3, :, :]
+    diffuse = torch.clamp(map_to_img(svbrdf[3:6, :, :]), min=0.0, max=1.0)
+    roughness = torch.clamp(map_to_img(svbrdf[6:9, :, :]), min=0.001, max=1.0)
+    specular = torch.clamp(map_to_img(svbrdf[9:12, :, :]), min=0.0, max=1.0)
+    wi_norm = norm_vec(wi)
+    wo_norm = norm_vec(wo)
+    wi_norm = torch.unsqueeze(torch.unsqueeze(wi_norm, -1), -1)  # [3,1,1]
+    wo_norm = torch.unsqueeze(torch.unsqueeze(wo_norm, -1), -1)  # [3,1,1]
+    h = norm_vec(torch.add(wi_norm, wo_norm)/2.0)  # [3,1,1]
+    NdotH = torch.mul(normal, h)
+    NdotL = torch.mul(normal, wi_norm)
+    NdotV = torch.mul(normal, wo_norm)
+    VdotH = torch.mul(wo_norm, h)
+    NdotH_pos = torch.clamp(NdotH, min=0.0)
+    NdotL_pos = torch.clamp(NdotL, min=0.0)
+    NdotV_pos = torch.clamp(NdotV, min=0.0)
+    VdotH_pos = torch.clamp(VdotH, min=0.0)
+
+    diffuse_rendered = diffuse * (1.0 - specular)/math.pi
+
+    # D
+    # alpha = torch.square(roughness)
+    alpha = roughness**2
+    # denominator = 1.0/torch.clamp((torch.square(NdotH_pos)*(torch.square(alpha)-1.0)+1.0), min=0.001)
+    denominator = 1.0/torch.clamp(((NdotH_pos**2)*((alpha**2)-1.0)+1.0), min=0.001)
+    # D = torch.square(alpha*denominator)/math.pi
+    D = ((alpha*denominator)**2)/math.pi
+
+    # F
+    sphg = torch.pow(((-5.55473*VdotH_pos)-6.98316)*VdotH_pos, exponent=2.0)
+    F = specular+(1.0-specular)*sphg
+
+    # G
+    # k = torch.square(roughness)/2.0
+    k = (roughness**2)/2.0
     g1 = 1.0/torch.clamp((NdotL_pos*(1.0-k)+k), min=0.001)
     g2 = 1.0/torch.clamp((NdotV_pos*(1.0-k)+k), min=0.001)
     G = g1 + g2
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from Model import *
 import time
-import PIL
+from PIL import Image
 from torchvision import transforms
 import numpy as np
 from torch.autograd import Variable
@@ -21,22 +21,42 @@ if __name__ == '__main__':
     to_img = transforms.ToPILImage()
     to_tensor = transforms.ToTensor()
 
-    dataset_path = '/media/winter/M2/UbuntuDownloads/Deschaintre2018_Dataset/Data_Deschaintre18/testBlended'
-    print('fetching data...')
-    dataset = MaterialDataset(data_dir=dataset_path)
-    data_loader = DataLoader(dataset, batch_size=8, shuffle=True, num_workers=4)
-    demo_batch = []
-    for i, img_batch in enumerate(data_loader):
-        if i == 0:
-            demo_batch = batch_to_tensor(img_batch[0])  # [8,15,288,288]
-    N, C_total, H, W = demo_batch.shape
-    svbrdf = demo_batch[:, 3:15, :, :]
-
-    lightpos = generate_direction(N)
-    viewpos = generate_direction(N)
-    result = tf_render(svbrdf, lightpos, viewpos)
+    img = Image.open('1.png')
+    img = to_tensor(img)
+    img = torch.unsqueeze(img, dim=0)
+    img_tensor = torch.squeeze(batch_to_tensor(img), dim=0)
+    svbrdf_single = img_tensor[3:, :, :]
+    lightpos = generate_direction(1)
+    viewpos = generate_direction(1)
+    lightpos = torch.squeeze(lightpos, dim=0)
+    viewpos = torch.squeeze(viewpos, dim=0)
+    result = single_renderer(svbrdf_single, lightpos, viewpos)
     print(result.shape)
-    img1 = to_img(result[1, :, :, :])
-    plt.imshow(img1)
+    rendered = to_img(result)
+    plt.imshow(rendered)
     plt.show()
+
+    # dataset_path = '/media/winter/M2/UbuntuDownloads/Deschaintre2018_Dataset/Data_Deschaintre18/testBlended'
+    # print('fetching data...')
+    # dataset = MaterialDataset(data_dir=dataset_path)
+    # data_loader = DataLoader(dataset, batch_size=8, shuffle=True, num_workers=4)
+    # demo_batch = []
+    # for i, img_batch in enumerate(data_loader):
+    #     if i == 0:
+    #         demo_batch = batch_to_tensor(img_batch[0])  # [8,15,288,288]
+    # N, C_total, H, W = demo_batch.shape
+    # svbrdf = demo_batch[:, 3:15, :, :]
+    #
+    # normal1 = svbrdf[0,0:3,:,:]
+    # img = to_img(normal1)
+    # plt.imshow(img)
+    # plt.show()
+    #
+    # lightpos = generate_direction(N)
+    # viewpos = generate_direction(N)
+    # result = tf_render(svbrdf, lightpos, viewpos)
+    # print(result.shape)
+    # img1 = to_img(result[1, :, :, :])
+    # plt.imshow(img1)
+    # plt.show()
 
